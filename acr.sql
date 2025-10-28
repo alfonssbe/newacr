eaef098ea5e4320203966cf294ab189453b9a7d8
alfonssbe <alfons@sbacoustics.com>
2025-10-28 16:18:19 +0700

    type NavbarFeaturedProduct still error - added sql

--- acr.sql
+++ acr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2025 at 10:25 AM
+-- Generation Time: Oct 28, 2025 at 10:17 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
